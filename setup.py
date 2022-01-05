520c7803bfb26787ae55547c2faca39a0e9f2ead
Grega Vrbančič <grega.vrbancic@gmail.com>
2022-01-05 14:27:21 +0100

    Fix tox.ini

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Testing',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
